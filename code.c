84b6a0051385c4934c988410cf4ca8ad5b7e9a14
iulianesanu07 <iulian.esanu1@gmail.com>
2025-01-08 09:51:34 +0100

    pleins de baballes mais pas de collision entre elles

--- code.c
+++ code.c
@@ -3,13 +3,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define PI 3.14159265
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
-#define G -9.8f   // Constante gravitationelle
-#define DT 0.008f // DeltaTime intervalle de temps (~120 FPS)
+#define G -9.8f         // Constante gravitationelle
+#define DT 0.008f       // DeltaTime intervalle de temps (~120 FPS)
+#define NBR_CIRCLES 25  // Nombre de cercles
 
+/* Declaration structure d'un Cercle */
 typedef struct {
   float px, py; // Position
   float vx, vy; // Velocity
@@ -17,11 +20,44 @@ typedef struct {
   int n;        // Resolution
 } *Circle;
 
-Circle c;
+/* Variables pour cercles */
+Circle c[NBR_CIRCLES];
+
+/* Variables pour physique des particules */
 float fa = 20;  // faster animations
 float cr = 0.7; // constante rebond
 
+/* Variables pour fps */
+int frameCount = 0;
+double previousTime = 0.0;
+double fps = 0.0;
 
+/* Prototypes */
+Circle initCircle(float px, float py, float vx, float vy, float r, int n);
+void calculateFPS();
+void drawCircle(Circle c);
+void initTabCircle(int n);
+void init();
+void textRender(int x, int y, char *text);
+void showData();
+void updatePos(Circle c);
+void display();
+void timer(int x);
+float generateRandomFloat(float min, float max);
+int randBetween(int min, int max);
+
+
+
+/* Main */
+
+
+float generateRandomFloat(float min, float max) {
+  return min + (float)rand() / RAND_MAX * (max - min);
+}
+
+int randBetween(int min, int max) {
+  return min + rand() % (max - min + 1);
+}
 
 Circle initCircle(float px, float py, float vx, float vy, float r, int n) {
   Circle c = malloc(sizeof(*c));
@@ -41,9 +77,7 @@ Circle initCircle(float px, float py, float vx, float vy, float r, int n) {
   return c;
 }
 
-int frameCount = 0;
-double previousTime = 0.0;
-double fps = 0.0;
+
 
 void calculateFPS() {
   double currentTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0; // temps en secondes
@@ -84,11 +118,28 @@ void drawCircle(Circle c) {
   glEnd();
 }
 
-void init(float vx, float vy) {
+void initTabCircle(int n) {
+  for (int i = 0; i < NBR_CIRCLES; i++) {
+    c[i] = initCircle(
+      generateRandomFloat(0, WIN_WIDTH),
+      generateRandomFloat(0, WIN_HEIGHT),
+      generateRandomFloat(-1000, 1000),
+      generateRandomFloat(-1000, 1000),
+      randBetween(5, 50),
+      25
+    );
+  }
+}
+
+void init() {
   glClearColor(0.0, 0.0, 0.0, 0);
   gluOrtho2D(0, WIN_WIDTH, 0, WIN_HEIGHT);
 
-  c = initCircle(400.0, 300.0, vx, vy, 20.0, 15);      // parametres initiaux
+  for (int i = 0; i < NBR_CIRCLES; i++) {
+    initTabCircle(i);
+  }
+
+//c[n] = initCircle(400.0, 300.0, vx, vy, 20.0, 15);      // parametres initiaux
 }
 
 void textRender(int x, int y, char *text) {
@@ -103,18 +154,22 @@ void showData() {
 
   char fpsText[32];
   sprintf(fpsText, "FPS : %d", (int)fps);
+/*  
   char pos[32];
   sprintf(pos, "X : %.2f Y : %.2f (pos)", c->px, c->py);
   char vel[64];
   sprintf(vel, "VelX : %.2f VelY : %.2f (px/s)", c->vx, c->vy);
+*/
 
   textRender(10, WIN_HEIGHT - 20, fpsText);
+/*
   textRender(10, WIN_HEIGHT - 35, pos);
   textRender(10, WIN_HEIGHT - 50, vel);
+*/
 }
 
 
-void updatePos() {
+void updatePos(Circle c) {
   // update velocity
   c->vy += (G * fa) * DT;
   c->vx += 1.0 * DT;
@@ -152,7 +207,9 @@ void display() {
 
   calculateFPS();
 
-  drawCircle(c);
+  for (int i = 0; i < NBR_CIRCLES; i++) {
+    drawCircle(c[i]);
+  }
 
   showData();
 
@@ -167,11 +224,16 @@ void display() {
 }
 
 void timer(int x) {
-  updatePos();
+
+  for (int i = 0; i < NBR_CIRCLES; i++) {
+    updatePos(c[i]);
+  }
+  
   glutPostRedisplay();
   glutTimerFunc(8, timer, 0);
 }
 
+/*
 void mouseClick(int button, int state, int x, int y) {
   if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON) {
     c->px = x;
@@ -181,27 +243,32 @@ void mouseClick(int button, int state, int x, int y) {
     glutPostRedisplay();
   }
 }
+*/
 
 int main(int argc, char *argv[]) {
 
   printf("Lancement programme :\n");
 
+  srand((unsigned int)time(NULL));
+
+  /*
   float vxI = strtof(argv[1], NULL);
   float vyI = strtof(argv[2], NULL);
 
   printf("argv[0] : %s\n", argv[0]);
   printf("argv[1] : %f\n", vxI);
   printf("argv[2] : %f\n", vyI);
+  */
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
-  glutInitWindowPosition(700, 20);
+  glutInitWindowPosition(500, -500);
   glutCreateWindow("Boing");
-  init(vxI, vyI);
+  init();
 
   glutTimerFunc(0, timer, 0); // Lance le timer
-  glutMouseFunc(mouseClick);
+//glutMouseFunc(mouseClick);
 
   glutDisplayFunc(display);
   
